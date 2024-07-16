84d4428492059b09f69b82189a7af42f771482a4
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-15 23:19:27 -0500

    constructores con nombres en dart

--- O1-DART-INTRO/06_class.dart
+++ O1-DART-INTRO/06_class.dart
@@ -1,12 +1,37 @@
 void main() {
-  final Hero wolverine = new Hero(name: 'Logan', power: 'Regeneration');
-  final Hero deadpool = new Hero(name: 'Deadpool');
+  // final Hero wolverine = new Hero(name: 'Logan', power: 'Regeneration');
+  // final Hero deadpool = new Hero(name: 'Deadpool');
+  // print(wolverine.toString());
+  // print(deadpool.toString());
 
-  print(wolverine.toString());
-  print(deadpool.toString());
+  final rawJson = {'name': 'Logan', 'power': 'Regeneration', 'isAlive': true};
+
+  final wolverin = Hero.fromJson(rawJson);
+  final ironman = new Hero(name: "Ironman ", power: "Money", isAlive: false);
+
+  print(ironman);
+  print(wolverin);
 }
 
 class Hero {
+  String name;
+  String power;
+  bool isAlive;
+
+  Hero({required this.name, required this.power, required this.isAlive});
+
+  Hero.fromJson(Map<String, dynamic> json)
+      : name = json['name'] ?? 'No name found',
+        power = json['power'] ?? 'No power found',
+        isAlive = json['isAlive'] ?? 'No isAlive found';
+
+  @override
+  String toString() {
+    return "Hero: $name, power: $power, isAlive:${isAlive ? "yes" : "nope"}";
+  }
+}
+
+/* class Hero {
   String name;
 
   String power;
@@ -29,3 +54,4 @@ class Hero {
     return 'Hero name: $name, power: $power';
   }
 }
+*/
