59a19426fa05a4129c6872e1729ae48ace911f52
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-16 23:59:44 -0500

    add file 07

---
+++ O1-DART-INTRO/07_getters_setters.dart
@@ -0,0 +1,26 @@
+void main() {
+  final mySquare = new Square(side: 5);
+ // mySquare.side = -8;
+  print(mySquare.area);
+}
+
+class Square {
+  double _side;
+
+  Square({required double side}) : _side = side;
+
+  double get area {
+    return _side * _side;
+  }
+
+  set side(double value) {
+    print('set value of site of square $_side to $value ');
+    if (value < 0) throw 'value must be >= 0';
+
+    _side = value;
+  }
+
+  double calculateArea() {
+    return _side * _side;
+  }
+}
