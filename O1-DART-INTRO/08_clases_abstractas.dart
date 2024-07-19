ec7f61937681424b2333574cc6ab0f1cbcd269b8
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-18 23:10:32 -0500

    new lines in 08 clases abstractas

--- O1-DART-INTRO/08_clases_abstractas.dart
+++ O1-DART-INTRO/08_clases_abstractas.dart
@@ -1,6 +1,15 @@
 void main() {
   final windPlant = new WindPlant(inicialEnery: 100.0);
-  print(windPlant); 
+  print(windPlant);
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLedt < 10) {
+    throw Exception("not enough energy");
+  }
+
+  return plant.energyLedt - 10;
 }
 
 enum PlantType { nuclear, wind, water }
