6bfe4e20425770a8bfcfe961e66ce59fc712f057
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-07-26 21:18:02 -0500

    Async y await en dart

---
+++ O1-DART-INTRO/11_Async - await.dart
@@ -0,0 +1,21 @@
+void main() async {
+  print("Inicio del programa ");
+
+  try {
+    final value = await httpGet("http://localhost");
+    print(value);
+  } catch (e) {
+    print("Error en la petición: $e");
+  }
+
+
+  print("Fin del programa ");
+}
+
+Future<String> httpGet(String url) async {
+  await Future.delayed(Duration(seconds: 1));
+  
+  throw "Error en la petición";
+
+  return "http://localhost";
+}
